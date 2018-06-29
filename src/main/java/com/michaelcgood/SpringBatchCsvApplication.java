03b2b71fa48072d756cea97ffbddfeb20743cb94
Hamsaleka <35956740+Hamsaleka@users.noreply.github.com>
2018-06-29 18:49:04 +0530

    first commit

---
+++ src/main/java/com/michaelcgood/SpringBatchCsvApplication.java
@@ -0,0 +1,22 @@
+package com.michaelcgood;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBatchCsvApplication {
+
+	public static void main(String[] args) {
+		System.out.println("******1");
+		SpringApplication.run(SpringBatchCsvApplication.class, args);
+	}
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/db/*");
+		return registration;
+	}
+}
